83b2a49f332ee8872d0f46d880ce944110d1ed48
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-02-26 16:57:10 +0200

    Add tip visibility to db and code

--- src/entities/tip.py
+++ src/entities/tip.py
@@ -1,6 +1,7 @@
 class Tip:
-    def __init__(self, title, url, description, user_id):
+    def __init__(self, title, url, description, user_id, visible):
         self.title = title
         self.url = url
         self.description = description
         self.user_id = user_id
+        self.visible = visible
--- src/repositories/tip_repository.py
+++ src/repositories/tip_repository.py
@@ -2,23 +2,23 @@ from src.database import database_connection
 from src.entities.tip import Tip
 
 def get_tip_by_row(row):
-    return Tip(row['title'], row['url'], row['description'], row['user_id']) if row else None
+    return Tip(row['title'], row['url'], row['description'], row['user_id'], row['visible']) if row else None
 
 class TipRepository:
     def __init__(self, connection):
         self.connection = connection
 
-    def add_tip(self, title, url, description, user_id):
+    def add_tip(self, title, url, description, user_id, visible=1):
         cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO tip (url, description, user_id, title)" \
-            " VALUES (?, ?, ?, ?)", (url, description, user_id, title))
+        cursor.execute("INSERT INTO tip (url, description, user_id, title, visible)" \
+            " VALUES (?, ?, ?, ?, ?)", (url, description, user_id, title, visible))
         self.connection.commit()
 
     def get_tips(self):
         """Palauttaa kaikki lukuvinkit listana"""
 
         cursor = self.connection.cursor()
-        sql = """SELECT title, url, description, user_id FROM Tip"""
+        sql = """SELECT title, url, description, user_id, visible FROM Tip"""
         cursor.execute(sql)
         rows = cursor.fetchall()
         return list(map(get_tip_by_row, rows))
