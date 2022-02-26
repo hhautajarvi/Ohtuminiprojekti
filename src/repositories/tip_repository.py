5eb674ceb0b70bbaacf47d63210871d88a7d62d5
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-02-26 15:45:05 +0200

    Add method to query all tips

--- src/repositories/tip_repository.py
+++ src/repositories/tip_repository.py
@@ -1,4 +1,8 @@
 from src.database import database_connection
+from src.entities.tip import Tip
+
+def get_tip_by_row(row):
+    return Tip(row['title'], row['url'], row['description'], row['user_id']) if row else None
 
 class TipRepository:
     def __init__(self, connection):
@@ -10,5 +14,13 @@ class TipRepository:
             " VALUES (?, ?, ?, ?)", (url, description, user_id, title))
         self.connection.commit()
 
+    def get_tips(self):
+        """Palauttaa kaikki lukuvinkit listana"""
+
+        cursor = self.connection.cursor()
+        sql = """SELECT title, url, description, user_id FROM Tip"""
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        return list(map(get_tip_by_row, rows))
 
 tip_repository = TipRepository(database_connection)
