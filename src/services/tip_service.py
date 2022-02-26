c9256c2d8c574d9b8e084237d5aca80d72880ab5
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-02-26 16:58:39 +0200

    Remove redundant code

--- src/services/tip_service.py
+++ src/services/tip_service.py
@@ -1,5 +1,4 @@
 from src.repositories.tip_repository import tip_repository as default_tip_repository
-from src.entities.tip import Tip
 
 class TipService:
     def __init__(self, tip_repository=default_tip_repository):
@@ -11,7 +10,6 @@ class TipService:
         if len(description) > 300:
             raise Exception("Anna enintään 300 merkin pituinen kuvaus")
         self._tip_repository.add_tip(title, url, description, user_id)
-        new_tip = Tip(title, url, description, user_id)
 
     def get_tips(self):
         tips = self._tip_repository.get_tips()
