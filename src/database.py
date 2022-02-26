4ad96103efa5334eb28312646079e9f688dcd633
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-02-26 15:28:13 +0200

    Use row_factory

--- src/database.py
+++ src/database.py
@@ -2,3 +2,4 @@ import sqlite3
 
 database_connection = sqlite3.connect("master.db", check_same_thread=False)
 database_connection.isolation_level = None
+database_connection.row_factory = sqlite3.Row
