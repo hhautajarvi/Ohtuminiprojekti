8ae1d6577d1ae5d27408096453eed66167e0e32f
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2022-02-28 11:09:08 +0200

    fix typo

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 import sqlite3
-from os import detenv
+from os import getenv
 
-database_connection = sqlite3.connect(detenv('DATABASE'), check_same_thread=False)
+database_connection = sqlite3.connect(getenv('DATABASE'), check_same_thread=False)
 database_connection.isolation_level = None
 database_connection.row_factory = sqlite3.Row
