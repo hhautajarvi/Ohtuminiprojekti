b8dcaf4bf25db6179d9698993a9ca3a6f5ca51e1
lina-sis <81074093+lina-ova@users.noreply.github.com>
2022-03-03 14:00:34 +0200

    Update run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+#alustetaan tietokantaa
 python src/initialize_database.py
 # käynnistetään Flask-palvelin taustalle
 export FLASK_APP=src/app
 flask run --port 5111 &
 
-# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5111/ping)" != "200" ]]; 
-  do sleep 1; 
-done
+
 
 # suoritetaan testit
 robot src/tests
