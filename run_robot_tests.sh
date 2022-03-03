fe078e625e2d0b92a568134dc684369511cadafa
Henrik Hautajärvi <henrik.hautajarvi@gmail.com>
2022-03-03 22:12:50 +0200

    fix bash script

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -3,7 +3,7 @@
 python src/initialize_database.py
 # käynnistetään Flask-palvelin taustalle
 export FLASK_APP=src/app
-flask run --port 5111 &
+flask run --port 5000 &
  
 
 # suoritetaan testit
@@ -12,6 +12,6 @@ robot src/tests
 status=$?
 
 # pysäytetään Flask-palvelin portissa 5000
-kill $(lsof -t -i:5111)
+kill $(lsof -t -i:5000)
 
 exit $status
