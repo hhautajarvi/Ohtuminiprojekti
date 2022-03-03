26ff329539356ba5c8f494232121fa38a106140c
lina-sis <81074093+lina-ova@users.noreply.github.com>
2022-03-03 13:56:18 +0200

    Update run_robot_tests.sh

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -2,10 +2,10 @@
 python src/initialize_database.py
 # käynnistetään Flask-palvelin taustalle
 export FLASK_APP=src/app
-flask run --port 5002 &
+flask run --port 5111 &
 
 # odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "200" ]]; 
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5111/ping)" != "200" ]]; 
   do sleep 1; 
 done
 
@@ -15,6 +15,6 @@ robot src/tests
 status=$?
 
 # pysäytetään Flask-palvelin portissa 5000
-kill $(lsof -t -i:5000)
+kill $(lsof -t -i:5111)
 
 exit $status
