94462eefe617f6c9abefa1c678530a198c194275
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-03-04 21:07:56 +0200

    Add user service tests

--- src/tests/services/user_service_test.py
+++ src/tests/services/user_service_test.py
@@ -2,10 +2,13 @@ import unittest
 from services.user_service import UserInputError, UserService
 from repositories.user_repository import UserRepository
 from database import database_connection
+from initialize_database import initialize_database
+from unittest.mock import patch
 
 
 class UserServiceTest(unittest.TestCase):
     def setUp(self):
+        initialize_database()
         self.user_repository = UserRepository(database_connection)
         self.user_service = UserService(self.user_repository)
 
@@ -23,3 +26,14 @@ class UserServiceTest(unittest.TestCase):
     def test_validate_passwords_dont_match(self):
         with self.assertRaises(UserInputError):
             self.user_service.validate("testinimi10", "testikayttajanimi10", "salasanaA", "salasanaB")
+
+    def test_incorrect_password_returns_error(self):
+        self.user_service.create_user("testinimi", "testikayttajanimi", "salasana", "salasana")
+        with self.assertRaises(UserInputError):
+            self.user_service.login("testikayttajanimi", "vaarasalasana")
+
+    @patch.object(UserService, "_set_session")
+    def test_correct_password_calls_set_session(self, mock):
+        self.user_service.create_user("testinimi", "testikayttajanimi", "salasana", "salasana")
+        self.user_service.login("testikayttajanimi", "salasana")
+        mock.assert_called()
